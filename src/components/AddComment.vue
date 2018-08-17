8c3f15194f0206d07875a5c685aa4d48c8714792
Aleksandar Babic <alekski64@icloud.com>
2018-08-17 10:58:24 +0200

    prvi dodatni zadatak

--- src/components/AddComment.vue
+++ src/components/AddComment.vue
@@ -1,32 +1,60 @@
 <template>
   <form @submit.prevent="onSubmit">
-    <div class="form-group">
-      <div class="col-6">
-        <textarea id="text" name="text" placeholder="Your comment here" class="form-control here" v-model="comment.text" required="required"></textarea>
+    <div class="add-comment-wrapper">
+      <div class="form-group md-6">
+          <textarea id="text" name="text" placeholder="Your comment here" class="form-control add-comment-input" v-model="comment.text" required="required"></textarea>
       </div>
-    </div>
-    <div class="form-group row">
-      <div class="offset-4 col-8">
-        <button name="submit" type="submit" class="btn btn-primary">Submit</button>
-      </div>
+      <custom-captcha @verify="handleCaptchaVerify"
+                      @expired="handleCaptchaExpired"
+                      ref="captcha"/>
+      <button name="submit" type="submit" class="btn btn-primary" :disabled="!captchaPassed">Submit</button>
     </div>
   </form>
 </template>
 
 <script>
+import CustomCaptcha from './shared/CustomCaptcha'
 export default {
-  data () {
+  components: {
+    CustomCaptcha
+  },
+  data() {
     return {
       comment: {
         text: ''
-      }
+      },
+      captchaPassed: false
     }
   },
 
   methods: {
-    onSubmit () {
+    onSubmit() {
+      if (!this.captchaPassed) {
+        return
+      }
+
       this.$emit('commentAdded', this.comment)
+      this.$refs.captcha.reset()
+    },
+    handleCaptchaVerify() {
+      this.captchaPassed = true
+    },
+    handleCaptchaExpired() {
+      this.captchaPassed = false
     }
   }
 }
 </script>
+
+<style>
+.add-comment-input {
+  width: 70%;
+}
+.add-comment-wrapper {
+  width: 35%;
+  padding: 10px;
+  margin: 10px;
+  border: 0.5px solid grey;
+  border-radius: 0.5rem;
+}
+</style>
---
+++ src/components/shared/CustomCaptcha.vue
@@ -0,0 +1,28 @@
+<template>
+    <vue-recaptcha sitekey="6Le-bmoUAAAAADgF0Xt3EqIdDr332AfG5lAviX6Z"
+                   @verify="onVerify"
+                   @expired="onExpired"
+                   @render="onRender"
+                   ref="captcha"/>
+</template>
+
+<script>
+import VueRecaptcha from 'vue-recaptcha'
+export default {
+  components: { VueRecaptcha },
+  methods: {
+    onVerify(data) {
+      this.$emit('verify', data)
+    },
+    onExpired(data) {
+      this.$emit('expired', data)
+    },
+    onRender(data) {
+      this.$emit('render', data)
+    },
+    reset() {
+      this.$refs.captcha.reset()
+    }
+  }
+}
+</script>
